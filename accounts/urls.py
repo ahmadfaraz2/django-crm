4caf50f5f9c4d8f3abcd35143a88a05cf575fed8
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-06-07 12:05:07 +0500

    Add forms and CRUD functionality on Orders

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/<int:pk>/', views.customer),
+    path('', views.home, name="home"),
+    path('products/', views.products, name="products"),
+    path('customer/<int:pk>/', views.customer, name="customer"),
+    path("create_order/", views.create_order, name="create_order"),
+    path("update_order/<int:pk>/", views.update_order, name="update_order"),
+    path("delete_order/<int:pk>/", views.delete_order, name="delete_order"),
 ]
