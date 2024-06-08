03778cd5ba25c83cc64b8d467184998da2b43968
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-06-08 18:14:09 +0500

    Work on authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
     path('customer/<int:pk>/', views.customer, name="customer"),
-    path("create_order/", views.create_order, name="create_order"),
+
+    path("create_order/<int:pk>/", views.create_order, name="create_order"),
     path("update_order/<int:pk>/", views.update_order, name="update_order"),
     path("delete_order/<int:pk>/", views.delete_order, name="delete_order"),
 ]
